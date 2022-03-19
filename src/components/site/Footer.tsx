c606aa165eac6d83a7795056230337cabceb9afa
Jose Carlos Sant Anna Junior <jcsj2010@gmail.com>
2022-03-18 21:51:01 -0300

    18.03.22

--- src/components/site/Footer.tsx
+++ src/components/site/Footer.tsx
@@ -1,5 +1,7 @@
-export default function Footer(){
-    return (
-        <div className="flex bg-orange-500 flex-shrink">FOOTER</div>
-    )
+export default function Footer() {
+  return (
+    <div className="flex items-center justify-center p-9 text-xl text-white bg-orange-400">
+      &reg; 2022
+    </div>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <div className="container flex items-center justify-center">
         <p className="">
           What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing
-          and typesetting industry. Lorem Ipsum has been the industry's standard
+          and typesetting industry. Lorem Ipsum has been the industrys standard
           dummy text ever since the 1500s, when an unknown printer took a galley
           of type and scrambled it to make a type specimen book. It has survived
           not only five centuries, but also the leap into electronic
